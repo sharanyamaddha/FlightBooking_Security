a1ffc2f81b1b62fd3237b6367cdbc9a3f8d255cb
Sharanya Maddha <sharanyamaddha@gmail.com>
2025-12-08 21:48:31 +0530

    adding controller

--- auth-service/src/main/java/com/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/authservice/controller/AuthController.java
@@ -1,5 +1,125 @@
 package com.authservice.controller;
 
+
+import com.authservice.dto.JwtResponse;
+import com.authservice.dto.LoginRequest;
+import com.authservice.dto.MessageResponse;
+import com.authservice.dto.SignupRequest;
+import com.authservice.model.ERole;
+import com.authservice.model.User;
+import com.authservice.repository.UserRepository;
+import com.authservice.security.JwtUtils;
+import com.authservice.service.UserDetailsImpl;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+//@CrossOrigin(origins = "*", maxAge = 3600) 
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    //  SIGNIN
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        JwtResponse jwtResponse = new JwtResponse(
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles
+        );
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .body(jwtResponse);
+    }
+
+    //  SIGNUP
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+      
+        List<String> strRoles = signUpRequest.getRoles();
+        Set<String> roles = new HashSet<>();
+
+        if (strRoles == null || strRoles.isEmpty()) {
+            roles.add(ERole.ROLE_USER.name());
+        } else {
+            strRoles.forEach(role -> {
+                if ("admin".equalsIgnoreCase(role)) {
+                    roles.add(ERole.ROLE_ADMIN.name());
+                } else {
+                    roles.add(ERole.ROLE_USER.name());
+                }
+            });
+        }
+
+        User user = new User();
+        user.setUsername(signUpRequest.getUsername());
+        user.setEmail(signUpRequest.getEmail());
+        user.setPassword(encoder.encode(signUpRequest.getPassword()));
+        user.setRoles(new ArrayList<>(roles));
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    //  SIGNOUT
+    @PostMapping("/signout")
+    public ResponseEntity<?> logoutUser() {
+        ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new MessageResponse("You've been signed out!"));
+    }
 }
