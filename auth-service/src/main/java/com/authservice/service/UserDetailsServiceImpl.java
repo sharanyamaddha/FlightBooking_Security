169d1ba2c889177016ecfa843f2c22d9490a39bf
Sharanya Maddha <sharanyamaddha@gmail.com>
2025-12-08 19:34:44 +0530

    added user service file

--- auth-service/src/main/java/com/authservice/service/UserDetailsServiceImpl.java
+++ auth-service/src/main/java/com/authservice/service/UserDetailsServiceImpl.java
@@ -1,5 +1,27 @@
 package com.authservice.service;
 
-public class UserDetailsServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.security.autoconfigure.SecurityProperties.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import com.authservice.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+		
+		User user=userRepository.findByUsername(username)
+				.orElseThrow(() ->
+                new UsernameNotFoundException("User Not Found with username: " + username));
+		
+        return UserDetailsImpl.build(user);
+
+	}
 
 }
