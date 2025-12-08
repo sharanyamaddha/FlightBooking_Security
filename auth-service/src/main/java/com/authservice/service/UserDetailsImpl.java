3b7ca19677ad8efcd686c6b85a765eb3301586de
Sharanya Maddha <sharanyamaddha@gmail.com>
2025-12-08 20:32:42 +0530

    added userDetails file

--- auth-service/src/main/java/com/authservice/service/UserDetailsImpl.java
+++ auth-service/src/main/java/com/authservice/service/UserDetailsImpl.java
@@ -1,5 +1,91 @@
 package com.authservice.service;
 
-public class UserDetailsImpl {
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.authservice.model.User;
+
+
+
+public class UserDetailsImpl implements UserDetails{
+
+	private String id;
+    private String username;
+    private String email;
+    private String password;
+    
+	private Collection<? extends GrantedAuthority> authorities;
+	
+	public UserDetailsImpl(String id,String username,String email,String password,
+			Collection<? extends GrantedAuthority> authorities) {
+		this.id = id;
+	    this.username = username;
+	    this.email = email;
+	    this.password = password;
+	    this.authorities = authorities;
+	}
+	
+	public static UserDetailsImpl build(User user) {
+		List<GrantedAuthority> authorities=user.getRoles().stream()
+				.map(SimpleGrantedAuthority::new)
+				.collect(Collectors.toList());
+		
+		 return new UserDetailsImpl(
+		            user.getId(),
+		            user.getUsername(),
+		            user.getEmail(),
+		            user.getPassword(),
+		            authorities
+		    );
+	}
+	
+	public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    // For now all account flags = true
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+    @Override
+    public boolean isEnabled() { return true; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserDetailsImpl that)) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
