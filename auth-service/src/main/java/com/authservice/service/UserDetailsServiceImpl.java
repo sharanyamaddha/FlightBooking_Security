1ab0f76173f61f0b6b16fc691dd1be163ce6e61b
Sharanya Maddha <sharanyamaddha@gmail.com>
2025-12-08 22:17:07 +0530

    updated service impl file

--- auth-service/src/main/java/com/authservice/service/UserDetailsServiceImpl.java
+++ auth-service/src/main/java/com/authservice/service/UserDetailsServiceImpl.java
@@ -1,27 +1,31 @@
 package com.authservice.service;
 
+
+
+import com.authservice.model.User;
+import com.authservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.security.autoconfigure.SecurityProperties.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.authservice.repository.UserRepository;
-
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService{
-	
-	@Autowired
-	UserRepository userRepository;
-	
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
-		
-		User user=userRepository.findByUsername(username)
-				.orElseThrow(() ->
-                new UsernameNotFoundException("User Not Found with username: " + username));
-		
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User Not Found with username: " + username)
+                );
+
         return UserDetailsImpl.build(user);
-
-	}
-
+    }
 }
+
